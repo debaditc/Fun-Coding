ac0bf85e83fd4c614ce35850ffa0beba93a11e0d
Debaditya Chakravorty <debaditya.chakravorty@gmail.com>
2016-12-26 13:20:26 -0500

    Add files via upload

---
+++ palindrome.scala
@@ -0,0 +1,35 @@
+/* Find palidromic no in given range without reverse command
+ * 121 , 222 ,111, 11 ..etc 
+ */
+import scala.collection.mutable.ArrayBuffer
+
+object palindrome {
+  
+  def main (args : Array[String])
+  {
+    println("Enter the range (0 to ?) : ")
+    val range = Console.readInt()
+    println("Range - " + range)
+    
+    
+  
+    for (i <- 0 to range)
+    {
+        var sum = ArrayBuffer[String]()
+        var rem = i
+        while(rem > 0)
+        {
+          var  frem = rem%10
+          sum += frem.toString()
+          rem = rem/10
+        } 
+        //println(sum.mkString + " ---- " + i.toString())
+        if(sum.mkString == i.toString())
+        {
+          println("Palindromic no - " + i) 
+        }
+        
+    }
+  }
+  
+}
